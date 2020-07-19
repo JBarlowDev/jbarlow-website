a120c19b22b9dabc751897d6b9d885aba86a5392
JBarlow <dev@jbarlow.uk>
2020-07-20 00:19:38 +0100

    Add header / footer with basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Header } from './components/header/header'
+import { Footer } from './components/footer/footer'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>The start</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import st from './header.module.scss'
+
+export const Header = () => {
+  return (
+    <div>
+      <p className={st.title}>Header</p>
+    </div>
+  )
+}
