3c32f550a97ca9667f6f37f2e6ddf22487af08d0
JBarlow <dev@jbarlow.uk>
2022-01-30 00:15:03 +0000

    Update dependencies

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import st from './header.module.scss'
 
 export const Header = () => {
   return (
     <div>
-      <p className={st.title}>Header</p>
+      <p>Header</p>
     </div>
   )
 }
