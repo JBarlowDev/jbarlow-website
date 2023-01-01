bdb4cffeb6f8aa794846d498944c5be19643340b
JBarlowDev <dev@jbarlow.uk>
2023-01-01 04:48:19 +0000

    Ignore org files

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -40,3 +40,4 @@ export const getStaticProps: GetStaticProps = async (props: GetStaticPropsContex
     }
   }
 }
+
--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -81,3 +81,4 @@ export async function getPostData(id: string | string[]) {
     metadata: matterResult.data
   }
 }
+
